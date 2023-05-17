61640ded5d3ff5ace64e7faeab2b4c2ce88edcd6
adiy55 <adiyafe55@gmail.com>
2023-05-17 17:29:01 +0300

    Support post request with multiple images + delete files on Exception

--- Resources/ImageHandler.py
+++ Resources/ImageHandler.py
@@ -25,24 +25,30 @@ class ImageHandler(Resource):
             abort(400, str(e))
 
     def post(self):
+        filenames = []
         try:
             data = request.json
-            encoded_image = data.get('image', None)
-            if encoded_image is None:
-                abort(400, "Bad image file")
+            image_array = data.get('images', None)
+            if image_array is None:
+                raise Exception("Missing images key in request!")
+            for encoded_image in image_array:
+                decoded_image = base64.b64decode(encoded_image)
 
-            decoded_image = base64.b64decode(encoded_image)
-            # Determine the file type dynamically using imghdr
-            image_extension = imghdr.what(None, h=decoded_image)
-            if image_extension is None:
-                raise ValueError("Unknown image format")
+                # Determine the file type dynamically using imghdr
+                image_extension = imghdr.what(None, h=decoded_image)
+                if image_extension is None:
+                    raise ValueError("Unknown image format")
 
-            if not os.path.exists(self.image_dir):
-                os.makedirs(self.image_dir)
-            unique_filename = f"{uuid.uuid4().hex}.{image_extension}"
-            image = Image.open(BytesIO(decoded_image))
-            image.save(os.path.join(self.image_dir, unique_filename), image_extension)
-            return {'filename': unique_filename}
+                if not os.path.exists(self.image_dir):
+                    os.makedirs(self.image_dir)
+                unique_filename = f"{uuid.uuid4().hex}.{image_extension}"
+                image = Image.open(BytesIO(decoded_image))
+                image.save(os.path.join(self.image_dir, unique_filename), image_extension)
+                filenames.append(unique_filename)
+
+            return {'filenames': filenames}
 
         except Exception as e:
+            for fn in filenames:
+                os.remove(os.path.join(self.image_dir, fn))
             abort(400, str(e))
